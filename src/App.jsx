eface79048ab592a858d5a1a9c525363064a90d8
joshuashea <joshshea44@gmail.com>
2023-08-17 19:25:08 -0700

    added sort form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Gallery from './Components/Gallery';
+import SortingForm from './Components/Form';
+
 
 function App() {
+  
+  
+  // const filterByHorns = (selectedHorns) => {
+    
+  // };
 
   return (
     
       <div>
         <Header />
+        <SortingForm /> {/* Pass down the function */}
         <Gallery />
         <Footer />
       </div>
